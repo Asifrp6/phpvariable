a5e05f9fea1cad514fba383eeb8c0c5f55f0e419
AsifRp <asifrp6@gmail.com>
2024-01-15 00:57:44 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $food="Mach";
+    $fooda="burger";
+
+
+    echo"I love ".$food;
+    ?>
+</body>
+</html>
